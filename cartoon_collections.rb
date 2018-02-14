b7afc1f7eca63b0cf878a048ee3979c2f3d5b0d7
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-14 19:19:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
     if
       array.include?(cheese_types)
-      return array.first(1)
+      return cheese_types.first(array)
     else
       nil
 end
